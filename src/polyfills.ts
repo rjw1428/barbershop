c28584b000739a027290105bb290c6bc2b193373
Ryan Wilk <rjw1428@gmail.com>
2023-02-26 22:39:24 -0500

    Update to angular 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,7 +55,7 @@ import smoothscroll from 'smoothscroll-polyfill';smoothscroll.polyfill();
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
